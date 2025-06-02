0fc938c7d909141f14a0e36045ddb24300bcdd6f
Pavlina Pavlova <pavlina.e.pavlova@gmail.com>
2025-06-02 10:51:50 +0200

    Fix CI build

---
+++ src/main/java/com/logmonitor/app/LogMonitorSpringBootApp.java
@@ -0,0 +1,11 @@
+package com.logmonitor.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogMonitorSpringBootApp {
+    public static void main(String[] args) {
+        SpringApplication.run(LogMonitorSpringBootApp.class, args);
+    }
+}
